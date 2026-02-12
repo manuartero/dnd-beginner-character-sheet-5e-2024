817f0d7178ede1e7b2b839110af4c30a25541cc1
Manuel Artero Anguita <manutero.developer@gmail.com>
2026-02-12 17:09:06 +0100

    chore: merge <AbilityCard/> component with display and creation modes

--- src/models/equipment.ts
+++ src/models/equipment.ts
@@ -1,14 +1,14 @@
 import type { DamageType } from "./damage-types";
 
 export type Equipment = {
-	name: string;
-	type: "weapon" | "armor" | "shield" | "gear";
-	attackBonus?: number;
-	damage?: {
-		dice: string;
-		type: DamageType;
-	};
-	ac?: number;
-	icon?: string;
-	properties?: string[];
+  name: string;
+  type: "weapon" | "armor" | "shield" | "gear";
+  attackBonus?: number;
+  damage?: {
+    dice: string;
+    type: DamageType;
+  };
+  ac?: number;
+  icon?: string;
+  properties?: string[];
 };
