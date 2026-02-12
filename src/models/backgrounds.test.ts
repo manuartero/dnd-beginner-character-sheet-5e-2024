e62dfa9c23b578c1a42e8e1eba11967fee0208c9
Manuel Artero Anguita <manutero.developer@gmail.com>
2026-02-12 21:10:56 +0100

    feat: origin with background icons

--- src/models/backgrounds.test.ts
+++ src/models/backgrounds.test.ts
@@ -1,7 +1,15 @@
-import { getSkillLabel } from "./backgrounds";
+import { getBackgroundIcon, getSkillLabel } from "./backgrounds";
 
 describe("getSkillLabel()", () => {
   it("should return the correct label for a given skill", () => {
     expect(getSkillLabel("athletics")).toBe("Athletics");
+  });
+});
+
+describe("getBackgroundIcon()", () => {
+  it("should return the correct icon path for a given background", () => {
+    expect(getBackgroundIcon("artisan")).toBe(
+      "/assets/vol5/BLACK/icon-vol5_81.svg",
+    );
   });
 });
