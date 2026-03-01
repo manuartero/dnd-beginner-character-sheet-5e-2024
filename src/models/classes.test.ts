298bfca556bd940cf91d576597e57aaf6c840660
Manuel Artero Anguita <manutero.developer@gmail.com>
2026-03-01 16:28:05 +0100

    chore: minor icon work

--- src/models/classes.test.ts
+++ src/models/classes.test.ts
@@ -2,6 +2,6 @@ import { getClassIcon } from "./classes";
 
 describe("getClassIcon()", () => {
   it("should return the correct icon path for a given character class", () => {
-    expect(getClassIcon("wizard")).toBe("/class-icons/wizard.jpeg");
+    expect(getClassIcon("wizard")).toBe("/class-icons/wizard.svg");
   });
 });
